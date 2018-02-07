6bb8722d1a73adb9a3fcf489c9ff8d5b2436ec54
abhiram moturi <abhirammoturi@abhirams-Air.wireless.yale.internal>
2018-02-07 13:40:14 -0500

    V2 in the works

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SignupForm from './Components/SignupForm';
 import AboutPage from './Components/AboutPage';
 import Footer from './Components/Footer';
 import {firebase} from './index'
-import HomePage from './Components/HomePage';
+import ProfilesContainer from './Components/HomePage';
 import LandingPage from './Components/LandingPage';
 import AfterSubmissionPage from './Components/AfterSubmissionPage';
 
@@ -19,7 +19,7 @@ class App extends Component {
         <Route exact={true} path={'/login'} component={LoginForm} />
         <Route exact={true} path={'/signup'} component={SignupForm} />
         <Route exact={true} path={'/about'} component={AboutPage} />
-        <Route exact={true} path={'/home'} component={HomePage} />
+        <Route exact={true} path={'/home'} component={ProfilesContainer} />
         <Route exact={true} path={'/'} component={LandingPage} />
         <Route exact={true} path={'/submitted'} component={AfterSubmissionPage} />        
         </div>
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,50 +1,137 @@
 import React from 'react';
-import {Form, Divider, Button, Segment, Message, Header, Embed} from 'semantic-ui-react';
+import {Form, Divider, Button, Image, Card, Message, Header, Embed, Icon, Grid} from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
+import {firebase} from '../index';
+import AfterSubmissionPage from './AfterSubmissionPage';
 
 class Profile extends React.Component {
+    state = {
+        liked: false,
+    }
+    
+    handleLikeButton = () => {
+        this.setState({liked: true});
+        this.props.addToListOfLikedProfiles(this.props.profile)
+    }
 
+    handleUnlikeButton = () => {
+        this.setState({liked: false});
+        this.props.removeFromListOfLikedProfiles(this.props.profile)
+    }
+
+    render(){
+        const {liked} = this.state
+        const profile = this.props.profile
+        return(
+            <Card>
+                <Image src={profile.profileImageUrl} />
+                <Card.Content>
+                <Card.Header>{profile.name}</Card.Header>
+                <Card.Meta>{profile.yaleAffiliation}</Card.Meta>
+                <Card.Description>{profile.desc}</Card.Description>
+                </Card.Content>
+
+                <Card.Content extra>
+                    {liked && <Button content='Liked' positive onClick={this.handleUnlikeButton}/>}
+                    {!liked && <Button content='Unliked' negative onClick={this.handleLikeButton}/>}
+                </Card.Content>
+            </Card>
+        )      
+    }
 }
 
-class ProfileContainer extends React.Component {
+class ProfilesContainer extends React.Component {
+    state = {
+        likedProfiles: [],
+        profiles: [],
+        didSubmitLikes: false,
+    }
+
+    handleLikeButton(){
+        
+    }
+
+    componentDidMount(){
+        const usersRef = firebase.database().ref().child('Users');
+        let newState = []
+        usersRef.on('value', (snapshot) => {
+            const profiles = snapshot.val();
+            for (let profile in profiles){
+                newState.push({
+                    id: profile,
+                    name: profiles[profile].name,
+                    desc: profiles[profile].description,
+                    yaleAffiliation: profiles[profile].yaleAffiliation,
+                    profileImageUrl: profiles[profile].profileImageUrl,                 
+                });
+            }
+            console.log(newState)
+            this.setState({
+                profiles: newState,
+            })
+        })
+    }
+
+    removeFromListOfLikedProfiles = (profile) => {
+        let newLikedProfiles = this.state.likedProfiles
+        newLikedProfiles.splice(newLikedProfiles.indexOf(profile),1)
+        this.setState({likedProfiles: newLikedProfiles})
+        console.log(this.state.likedProfiles)
+    }
+
+    addToListOfLikedProfiles = (profile) => {
+        if (this.state.likedProfiles.length == 10){
+            console.log('you already liked 10 profiles.');
+            return;
+        }
+        let newLikedProfiles = this.state.likedProfiles
+        newLikedProfiles.push(profile)
+        this.setState({likedProfiles: newLikedProfiles})
+        console.log(this.state.likedProfiles)
+    }
+
+    onSubmitClick = () => {
+        const currentUserId = firebase.auth().currentUser.uid;
+        var userRef = firebase.database().ref().child('Users').child(currentUserId)
+        userRef.child('likes').set(true);
+        for (var i=0; i<this.state.likedProfiles.length;i++){
+            console.log(this.state.likedProfiles[i])
+            userRef.child('likes').child(this.state.likedProfiles[i].id).set(true)
+        }
+        userRef.child('didSubmitLikes').set(true);
+        alert('Done submitting likes!');
+        this.setState({
+            didSubmitLikes: true,
+        })
+    }
+
     render(){
+        const profiles = this.state.profiles
+        console.log(profiles)
+        if (profiles.length > 0 && !this.state.didSubmitLikes) {
         return (
             <div>
-                
+            {profiles.map((profile) => {
+                return (
+                    <Profile 
+            addToListOfLikedProfiles={this.addToListOfLikedProfiles}
+            removeFromListOfLikedProfiles={this.removeFromListOfLikedProfiles}
+            profile={profile}/>
+                )
+            })}
+            <Button positive content='submit' onClick={this.onSubmitClick}/>
             </div>
-        );
+            
+            );
+    }
+    else if (this.state.didSubmitLikes){
+        return(<AfterSubmissionPage/>);
+    }
+    else {
+        return(<div></div>)
+    }
     }
 }
 
-var AboutPost = () => {
-    return (
-        <div class="ui column stackable centered page grid">
-            <div class="column wide">
-                <Segment>
-                    <Header>
-                        Home Page is still under construction. We are working round the clock.
-                    </Header>
-                    <Divider/>
-                    <Embed
-                        defaultActive
-                        url='https://giphy.com/embed/JIX9t2j0ZTN9S'/>
-                    <Divider/>
-                    <p> Please <a href="/signup"> sign up </a> in the meantime </p>
-                    <p> For more info please <a href="mailto:abhi.moturi@yale.edu?Subject=[The Y List]Heythere!"> contact us </a></p>
-                </Segment>
 
-                <Button onClick={()=>(window.location='/submitted')}> Submit </Button>
-            </div>
-        </div>
-    )
-}
-
-class HomePage extends React.Component {
-    render(){
-        return(
-            <AboutPost/>
-        )
-    }
-}
-
-export default HomePage;
+export default ProfilesContainer;
