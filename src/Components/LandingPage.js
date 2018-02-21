6546e6e304aee7148d90c6d69c09c138949a4aaf
abhiramee08b021 <abhiramee08b021@gmail.com>
2018-02-21 10:32:42 -0500

    Update LandingPage.js

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -14,7 +14,7 @@ var LandingPageSegment = () => {
                         Why be on Tinder, CMB, Hinge, Bumble, League, Match, OkCupid, 
                         Tastebuds, Happn, Zoosk when the people you wanna date are around you?
                     </p>
-                    <p>Sign up for a chance to match with someone on campus for Valentine’s Day!
+                    <p>Sign up for a chance to match with someone on campus!
                        You can enter up to 10 people you’d be willing to go on a date with and 
                        find out if they feel the same! Your name will only be revealed to the 
                        people who put you on their list as well.
